4a89348271b6b6fe1802c616f06a8b77c05891ad
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-05 20:25:03 -0600

    Documentando funciones y tipos de valores

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-var n = 59 // Enteros
+// Números
 
-var d = 0.909 // Decimales
+var n = 59; // Enteros
+var d = 0.909; // Decimales
 
-var i = Infinity
-var inn = -Infinity
-var n = NaN
+// Números especiales, resultados de operaciones sin sentido
 
-// arrays
+var i = Infinity;
+var inn = -Infinity;
+var n = NaN;
 
-var  a = ["hola" , "5", 9, "00"]
+// Arrays: colecciones de elementos, ordenadas
 
-var elementos = ["piedra", "papel", "tijeras"]
+var  aleatorio = ["hola" , "5", 9, "00"];
+var elementos = ["piedra", "papel", "tijeras"];
 
-Math.ceil(5.5)
-Math.floor(5.5)
-Math.round(5.5)
-Math.random()
+// Consultar elementos en el order, el primer elemento es 0
+console.log(elementos[0])
+console.log(elementos[1])
+console.log(elementos[2])
+
+
+// Operaciones con Arrays
+elementos.push("lizard"); // Añadir un elemento al final del array
+elementos.pop(); // Remueve y devuelve el último elemento del array
+
+
+// Operaciones Matemáticas
+
+Math.ceil(5.5); // Redondea siempre hacia arriba
+Math.floor(5.5); // Redondea siempre hacia abajo
+Math.round(5.5); // Redondea dependiendo del decimal .5 hacia arriba, .4 hacia abajo
+Math.random(); // Devuelve un número aleatorio entre 0 y 1
 
 
 
