38f9267864dbe5c3179e4aa942a06708c3bbc632
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-05 20:15:57 -0600

    Variables and arrays

---
+++ script.js
@@ -0,0 +1,21 @@
+var n = 59 // Enteros
+
+var d = 0.909 // Decimales
+
+var i = Infinity
+var inn = -Infinity
+var n = NaN
+
+// arrays
+
+var  a = ["hola" , "5", 9, "00"]
+
+var elementos = ["piedra", "papel", "tijeras"]
+
+Math.ceil(5.5)
+Math.floor(5.5)
+Math.round(5.5)
+Math.random()
+
+
+
