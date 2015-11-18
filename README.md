033f223c44ae566acf17579b7357362117b870e0
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-17 21:25:51 -0600

    Creando página

---
+++ README.md
@@ -0,0 +1,5 @@
+Ejercicios de JavaScript
+========
+
+Aquí se subirán los ejercicios de JavaScript para las clases.
+
---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Números
+
+var n = 59; // Enteros
+var d = 0.909; // Decimales
+
+// Números especiales, resultados de operaciones sin sentido
+
+var i = Infinity;
+var inn = -Infinity;
+var n = NaN;
+
+// Arrays: colecciones de elementos, ordenadas
+
+var  aleatorio = ["hola" , "5", 9, "00"];
+var elementos = ["piedra", "papel", "tijeras"];
+
+// Consultar elementos en el order, el primer elemento es 0
+console.log(elementos[0])
+console.log(elementos[1])
+console.log(elementos[2])
+
+
+// Operaciones con Arrays
+elementos.push("lizard"); // Añadir un elemento al final del array
+elementos.pop(); // Remueve y devuelve el último elemento del array
+
+
+// Operaciones Matemáticas
+
+Math.ceil(5.5); // Redondea siempre hacia arriba
+Math.floor(5.5); // Redondea siempre hacia abajo
+Math.round(5.5); // Redondea dependiendo del decimal .5 hacia arriba, .4 hacia abajo
+Math.random(); // Devuelve un número aleatorio entre 0 y 1
+
+
+
