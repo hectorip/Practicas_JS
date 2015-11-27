c364cdaac07d26e87d8e3672a89c05cf894125bb
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2015-11-26 23:58:29 -0600

    Apuntes Programción Orientada a Objetos y DOM básico

---
+++ apuntes_dom.js
@@ -0,0 +1,36 @@
+
+/* Clases -> Conjunto objetos que comparten caractrísticas
+Objetos -> Concreción de las clases.
+    -> propiedades (cosas que tiene o que es)
+    -> métodos (cosas que hace)
+
+Obejeto.propiedad
+Obejeto.metodo()
+
+
+Perro.num_patas
+Perro.color
+
+Perro.ladrar()
+
+
+*/
+
+
+Math.random()
+//  Exacto
+
+console.log("asdas")
+
+// Seleccionar sobre que se va actuar
+area = document.querySelector("#mi_area")
+
+// Crear elemento (guardar en variable)
+el = document.createElement("a")
+
+
+// Modificar Elemeto
+el.innerText = "Hola"
+
+// Insertarlo
+area.appendChild(el)
